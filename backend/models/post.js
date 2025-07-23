e71b3ea0d96dc1bd28a76033e71bd794300601e1
Kripan <krpnspkt@gmail.com>
2025-07-23 14:48:00 +0545

    added post in models

---
+++ backend/models/post.js
@@ -0,0 +1,46 @@
+import { sequelize } from "../config/db.js";
+import { DataTypes } from "sequelize";
+
+const Post = sequelize.define("Request", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "pending",
+  },
+  requestedBy: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  assignedTo: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  skillsRequired: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default Post;
